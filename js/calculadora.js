f30baaac1bceeda6dcc2941ba5ce92ad2f2d3945
Francine Santos <francinesanto@gmail.com>
2023-11-30 10:02:48 -0300

    primeira finalização calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,18 +3,70 @@
 2. Exibir o resultado na tela
 */
 
-function somar() {
+const botaoSubtrair = document.querySelector('#botaoSub')
+const botaoMult = document.querySelector('#botaoMult')
+const botaoDiv = document.querySelector('#botaoDiv')
 
+function somar() {
+    const num1 = Number(document.querySelector('#numero1Soma').value)
+    const num2 = Number(document.querySelector('#numero1Soma').value)
+
+    const total = num1 + num2
+
+    document.querySelector('.resultado').setAttribute('id','resultadoSoma')
+
+    const paragrafo = document.querySelector('#resultadoSoma')
+    const resultado = document.createElement('strong')
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
+    
 }
+
+
 
 function subtrair() {
+    const num1 = Number(document.querySelector('#numero1Sub').value)
+    const num2 = Number(document.querySelector('#numero2Sub').value)
+
+    const total = num1 - num2
+
+    const paragrafo = document.querySelector('#resultadoSub')
+    const resultado = document.createElement('strong')
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
 
 }
 
-function multiplicar() {
 
+
+
+function multiplicar() {
+    const num1 = Number(document.querySelector('#numero1Mult').value)
+    const num2 = Number(document.querySelector('#numero2Mult').value)
+
+    const total = num1 * num2
+
+    const paragrafo = document.querySelector('#resultadoMult')
+    const resultado = document.createElement('strong')
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
+    
 }
 
 function dividir() {
+    const num1 = Number(document.querySelector('#numero1Div').value)
+    const num2 = Number(document.querySelector('#numero2Div').value)
+
+    const total = num1 / num2
+
+    const paragrafo = document.querySelector('#resultadoDiv')
+    const resultado = document.createElement('strong')
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
 
 }
+
+
+botaoSubtrair.addEventListener('click', subtrair)
+botaoMult.addEventListener('click', multiplicar)
+botaoDiv.addEventListener('click', dividir)
